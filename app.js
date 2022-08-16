3a84f911881cfa0f3cad195bdd9724c6b5619593
Chlotte <charlottealleaume99@gmail.com>
2022-08-16 14:57:30 +0200

    setup

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 /**
  *  Your code ⬇️
  */
+
+const express = require('express');
+const app = express();
+
+const data = require('./disney.json');
+
+app.get('/', (request, response) => {
+	response.send("Hello world!");
+});
+
+app.listen(3000);
+
